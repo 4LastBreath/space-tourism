75bb8e93c780595da91379d75af82ef4b832f068
4LastBreath <04lastbreath@gmail.com>
2024-12-28 13:07:30 +0100

    test cache img bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/routes'
-
+import { useEffect, useState } from 'react'
+import { imgs } from './constants/imgs'
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  const cacheImages = async (imgs:string[]) => {
+    const promises = imgs.map((src) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image()
+
+        img.src = src
+        img.onload = resolve
+        img.onerror = reject
+      })
+    })
+
+    await Promise.all(promises)
+
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    cacheImages(imgs)
+  }, [])
+
   return (
-    <RouterProvider router={router} />
+    <>
+      {isLoading ?
+        <div className='h-screen w-full flex align-center justify-center'>
+          Loading...
+        </div>
+        :
+        <RouterProvider router={router} />
+      }
+    </>
   )
 }
 
---
+++ src/constants/imgs.ts
@@ -0,0 +1,27 @@
+import homeDesktopBg from '../styles/assets/home/background-home-desktop.jpg'
+import homeTabletBg from '../styles/assets/home/background-home-tablet.jpg'
+import homeMobileBg from '../styles/assets/home/background-home-mobile.jpg'
+import crewDesktopBg from '../styles/assets/crew/background-crew-desktop.jpg'
+import crewTabletBg from '../styles/assets/crew/background-crew-tablet.jpg'
+import crewMobileBg from '../styles/assets/crew/background-crew-mobile.jpg'
+import destinationDesktopBg from '../styles/assets/destination/background-destination-desktop.jpg'
+import destinationTabletBg from '../styles/assets/destination/background-destination-tablet.jpg'
+import destinationMobileBg from '../styles/assets/destination/background-destination-mobile.jpg'
+import technologyDesktopBg from '../styles/assets/technology/background-technology-desktop.jpg'
+import technologyTabletBg from '../styles/assets/technology/background-technology-tablet.jpg'
+import technologyMobileBg from '../styles/assets/technology/background-technology-mobile.jpg'
+
+export const imgs = [
+  homeDesktopBg,
+  homeTabletBg,
+  homeMobileBg,
+  crewDesktopBg,
+  crewTabletBg,
+  crewMobileBg,
+  destinationDesktopBg,
+  destinationTabletBg,
+  destinationMobileBg,
+  technologyDesktopBg,
+  technologyTabletBg,
+  technologyMobileBg
+]
